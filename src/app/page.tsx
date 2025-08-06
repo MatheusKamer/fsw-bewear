a3e891966b5007c5c0d975f2cf1ae129f30d7f77
Matheus Kamer <matheus_kamer@hotmail.com>
2025-08-05 20:56:54 -0400

    chore: updated shadcn theme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,5 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 
 export default function Home() {
-  return (
-    <Button className={cn("border-blue-500 bg-blue-500 text-white")}>
-      BEWEAR
-    </Button>
-  );
+  return <Button>BEWEAR</Button>;
 }
