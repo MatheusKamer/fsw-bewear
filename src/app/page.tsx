5d8b368a3c36b2175e4478becd1c6e4600a980b9
Matheus Kamer <matheus_kamer@hotmail.com>
2025-08-07 20:57:39 -0400

    feat: add product listing and banner components, implement currency formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,45 @@
-import { Header } from '@/components/common/header';
+import Image from 'next/image';
 
-export default function Home() {
+import { Header } from '@/components/common/header';
+import { ProductsList } from '@/components/common/products-list';
+import { db } from '@/db';
+
+export default async function Home() {
+  const products = await db.query.productTable.findMany({
+    with: {
+      variants: true,
+    },
+  });
+  console.log(products);
+
   return (
     <>
       <Header />
+      <div className="space-y-6">
+        <div className="px-5">
+          <Image
+            src={'/banners/banner-01.png'}
+            alt="Live a life in style"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+
+        <ProductsList title="Featured Products" products={products} />
+
+        <div className="px-5">
+          <Image
+            src={'/banners/banner-02.png'}
+            alt="Live a life in style"
+            height={0}
+            width={0}
+            sizes="100vw"
+            className="h-auto w-full"
+          />
+        </div>
+      </div>
     </>
   );
 }
