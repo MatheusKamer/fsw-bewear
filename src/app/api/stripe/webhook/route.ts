3ac6d5dd7c5c7f2ee60df4f46fea7bb281d45f57
Matheus Kamer <matheus_kamer@hotmail.com>
2025-08-12 15:29:18 -0400

    feat: add stripe webhook route

---
+++ src/app/api/stripe/webhook/route.ts
@@ -0,0 +1,45 @@
+import { eq } from 'drizzle-orm';
+import { NextResponse } from 'next/server';
+import Stripe from 'stripe';
+
+import { db } from '@/db';
+import { orderTable } from '@/db/schema';
+
+export const POST = async (request: Request) => {
+  if (!process.env.STRIPE_SECRET_KEY || !process.env.STRIPE_WEBHOOK_SECRET) {
+    throw new Error('Stripe secret key or webhook secret is not defined');
+  }
+
+  const signature = request.headers.get('stripe-signature');
+
+  if (!signature) {
+    throw new Error('Missing Stripe signature');
+  }
+
+  const text = await request.text();
+
+  const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+  const event = stripe.webhooks.constructEvent(
+    text,
+    signature,
+    process.env.STRIPE_WEBHOOK_SECRET,
+  );
+
+  if (event.type === 'checkout.session.completed') {
+    const session = event.data.object as Stripe.Checkout.Session;
+    const orderId = session.metadata?.orderId;
+
+    if (!orderId) {
+      return NextResponse.error();
+    }
+
+    await db
+      .update(orderTable)
+      .set({
+        status: 'confirmed',
+      })
+      .where(eq(orderTable.id, orderId));
+  }
+
+  return NextResponse.json({ received: true });
+};
