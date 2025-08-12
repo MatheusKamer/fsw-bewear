2f7e82232571dee1f26d4e9510f3eb3a78f33fe6
Matheus Kamer <matheus_kamer@hotmail.com>
2025-08-12 15:32:52 -0400

    refactor: remove dialog confirmation from finish order button

--- src/app/cart/confirmation/components/finish-order-button.tsx
+++ src/app/cart/confirmation/components/finish-order-button.tsx
@@ -1,24 +1,12 @@
 'use client';
 import { loadStripe } from '@stripe/stripe-js';
 import { Loader2 } from 'lucide-react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useState } from 'react';
 
 import { createCheckoutSession } from '@/actions/create-checkout-session';
 import { Button } from '@/components/ui/button';
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogTitle,
-} from '@/components/ui/dialog';
 import { useFinishOrder } from '@/hooks/mutations/use-finish-order';
 
 export const FinishOrderButton = () => {
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-
   const { mutateAsync: finishOrderMutation, isPending } = useFinishOrder();
 
   const handleFinishOrder = async () => {
@@ -40,8 +28,6 @@ export const FinishOrderButton = () => {
       }
 
       await stripe.redirectToCheckout({ sessionId: checkout.id });
-
-      setIsDialogOpen(true);
     } catch (error) {
       console.error('Error finishing order:', error);
     }
@@ -57,26 +43,6 @@ export const FinishOrderButton = () => {
       >
         {isPending && <Loader2 className="h-4 w-4 animate-spin" />} Purchase
       </Button>
-      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
-        <DialogContent className="flex flex-col items-center justify-center space-y-2">
-          <Image
-            src="/order-finished.svg"
-            alt="Confirmation"
-            width={200}
-            height={200}
-          />
-          <DialogTitle className="text-2xl">Order Completed!</DialogTitle>
-          <DialogDescription className="text-center">
-            Thank you for your purchase! Your order has been successfully
-            completed.
-          </DialogDescription>
-          <DialogFooter>
-            <Button className="rounded-full" size={'lg'}>
-              <Link href="/">Return to the store</Link>
-            </Button>
-          </DialogFooter>
-        </DialogContent>
-      </Dialog>
     </>
   );
 };
